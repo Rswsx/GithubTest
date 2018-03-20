1d944497a1404399b086a4573c4db9866bfd2e92
勘测院遥感分院 <kanceyuanyaoganfenyuan@kanceyuanyaoganfenyuandeMac-mini.local>
2018-03-20 09:43:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kanceyuanyaoganfenyuan/githubtest/MyTool.java
@@ -0,0 +1,11 @@
+package com.example.kanceyuanyaoganfenyuan.githubtest;
+
+/**
+ * Created by kanceyuanyaoganfenyuan on 18/3/19.
+ */
+
+public class MyTool {
+    public static String getName(){
+        return "cesss";
+    }
+}
